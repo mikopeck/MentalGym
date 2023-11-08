261ed3450787f39806aa494f5e870c6fce6e3336
mikopeck <mikopeck@gmail.com>
2023-11-08 23:22:29 +0100

    idkanymore

--- startup.sh
+++ startup.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# Activate the virtual environment
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Start Gunicorn with the specified number of workers
 gunicorn --workers=3 --bind=0.0.0.0:8000 --chdir backend app:app
