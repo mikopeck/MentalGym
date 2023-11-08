cab23ac937e44f14f5cb883772b47bf1f9a25b72
mikopeck <mikopeck@gmail.com>
2023-11-08 19:13:12 +0100

    last test

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-cd backend
-source venv/bin/activate
-
-# Start Gunicorn with the specified number of workers
-gunicorn --workers=1 --bind=0.0.0.0:8000 app:app
