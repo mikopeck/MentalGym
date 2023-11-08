c211db6c410e6904ce2fc411cfec32b42238aa2f
mikopeck <mikopeck@gmail.com>
2023-11-08 20:01:37 +0100

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Activate the virtual environment
+source venv/bin/activate
+
+# Start Gunicorn with the specified number of workers
+gunicorn --workers=3 --bind=0.0.0.0:8000 --chdir backend app:app
