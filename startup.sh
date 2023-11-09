7e711870617bbd8c54bc8fe2bbde28fdfee175ba
mikopeck <mikopeck@gmail.com>
2023-11-09 16:34:40 +0100

    dbasvcaV

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+flask db upgrade
 
 # Start Gunicorn with the specified number of workers
 gunicorn --workers=3 --bind=0.0.0.0:8000 --chdir backend app:app
