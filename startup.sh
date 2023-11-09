28f622b6197c85f3ae63ddf46938cc6c5b9caa5c
mikopeck <mikopeck@gmail.com>
2023-11-09 21:08:06 +0100

    ummrly

--- startup.sh
+++ startup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+flask db upgrade
+
+flask shell <<EOF
+from sqlalchemy import MetaData
+meta = MetaData(bind=db.engine)
+meta.reflect()
+for table in meta.tables.values():
+    print(table)
+EOF
 
 # Start Gunicorn with the specified number of workers
 gunicorn --workers=3 --bind=0.0.0.0:8000 --chdir backend app:app
